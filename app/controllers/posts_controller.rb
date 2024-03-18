03a88ca256fb273642eb092ae10571759aaa8e14
Facundo Quiroga <facu.quirogaga@gmail.com>
2024-03-18 12:40:20 -0500

    Agregadas las autentificaciones, y autorizaciones

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,16 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
 
 
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc)
   end
 
 
   def show
-    @post = Post.find(params[:id])
+    @post
   end
 
 
@@ -17,9 +19,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to @post, notice: 'Post created.'
+      redirect_to @post, notice: 'Post creado exitosamente.'
     else
       render :new
     end
@@ -27,35 +29,52 @@ class PostsController < ApplicationController
 
 
   def edit
-    @post = Post.find(params[:id])
+    @post
+    unless @post.user == current_user
+      flash[:alert] = "No tienes permiso para editar este post."
+      redirect_to @post
+    end
   end
 
 
   def update
-    @post = Post.find(params[:id])
-    if @post.update(post_params)
-      redirect_to @post, notice: 'Post actualizado exitosamente.'
+    @post
+    unless @post.user == current_user
+      flash[:alert] = "No tienes permiso para editar este post."
+      redirect_to @post
     else
-      render :edit
+      if @post.update(post_params)
+        redirect_to @post, notice: 'Post actualizado exitosamente.'
+      else
+        render :edit
+      end
     end
   end
 
 
 
   def destroy
-    @post = Post.find(params[:id])
+    @post
     @post.destroy
     redirect_to posts_path, notice: 'Post eliminado exitosamente.'
   end
 
   def user_posts
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = @user.posts.order(created_at: :desc)
   end
 
   private
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def authorize_user!
+    redirect_to root_path, alert: 'No tienes autorización para realizar esta acción.' unless current_user == @post.user
   end
 end
