f3cb35e74cba1754b800ee4fe5a025fc2b8c2715
Facundo Quiroga <facu.quirogaga@gmail.com>
2024-03-25 18:35:34 -0500

    El formulario esta enriquecido con la posibilidad de cargar y mostrar imagenes en la publicacion

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
+  has_rich_text :description
   belongs_to :user
+  has_many_attached :images 
+
 
   validates :title, presence: true
   validates :content, presence: true
