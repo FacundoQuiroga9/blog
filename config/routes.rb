90c65946b0da631e5d356332c4de8146336e9c3b
Facundo Quiroga <facu.quirogaga@gmail.com>
2024-03-27 14:55:18 -0500

    Creada la ruta para la landing page e importado el carousel de bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   devise_for :users, controllers: { registrations: 'registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "posts#index"
+  root "pages#home"
   resources :posts
 
   resources :users do
