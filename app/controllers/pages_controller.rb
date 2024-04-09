bfc900c4a06a5bd56cf18f9d3bec906c3f8009be
Facundo Quiroga <facu.quirogaga@gmail.com>
2024-04-09 14:12:40 -0500

    Creada la pagina de staff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,18 @@ class PagesController < ApplicationController
   end
 
   def staff
-
+    @staff_members = [
+      { name: "Kyran Culhane", position: "Presidente", email: "juan@example.com", class: "kyran"},
+      { name: "Gabriel Valiente", position: "Coordinador de futbol", email: "juan@example.com", class: "gabriel"},
+      { name: "Ariel Argüello", position: "Marketing", email: "juan@example.com", class: "ariel"},
+      { name: "Facundo Quiroga", position: "Webmaster", email: "juan@example.com", class: "facundo"},
+      { name: "Santiago Salituro", position: "Social media", email: "juan@example.com", class: "santiago"}
+    ]
   end
 
+
   def team
-    
+
   end
 
   def contact
