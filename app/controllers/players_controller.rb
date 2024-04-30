07d2b396c208052c235c43cef62df60008133243
Facundo Quiroga <facu.quirogaga@gmail.com>
2024-04-30 14:15:39 -0500

    Agregada la busqueda de jugadores en el index_players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,6 +3,8 @@ class PlayersController < ApplicationController
   before_action :set_player, only: [:show, :edit, :update, :destroy]
 
   def index
+    @players = Player.all
+    @players = @players.where("name ILIKE ?", "%#{params[:query]}%") if params[:query].present?
     @players = order_players(params[:order], params[:direction])
     @matches = Match.order(date: :desc).page(params[:page]).per(1)
   end
@@ -51,17 +53,14 @@ class PlayersController < ApplicationController
 
     def order_players(order, direction)
       direction = %w[asc desc].include?(direction) ? direction : "asc"
-
       case order
       when "name", "games", "goals", "debut"
-        Player.all.order(order => direction)
+        @players = @players.order(order => direction)
       when "birthday"
-        # Ordena directamente por la fecha de nacimiento.
-        # Si 'asc' se usa, los más jóvenes (nacidos más recientemente) primero.
-        # Si 'desc' se usa, los más viejos primero.
-        Player.all.order(birthday: direction)
+        @players = @players.order(birthday: direction)
       else
-        Player.all.order(:name)
+        @players = @players.order(:name)
       end
+      @players
     end
 end
